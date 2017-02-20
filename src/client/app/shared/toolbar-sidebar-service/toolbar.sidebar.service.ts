05e9acd2357be614ddfec29f679979ff4f2ab79c
Adam Ben Shmuel <bsadambs@gmail.com>
2017-02-20 19:45:27 +0200

    Add service for communication between toolbar and sidebar

---
+++ src/client/app/shared/toolbar-sidebar-service/toolbar.sidebar.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, Inject } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+@Injectable()
+export class ToolbarSidebarService {
+  private notify = new Subject<any>();
+
+  notifyObservable$ = this.notify.asObservable();
+
+  constructor(){}
+
+  public  notifyOther(data: any) {
+    if (data) {
+      this.notify.next(data);
+    }
+  }
+}
